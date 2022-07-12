848eabf05433488e060a282e465ea223124b5873
christinxxv <cwilson39@missuouriwestern.edu>
2022-07-12 18:48:05 -0500

    in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './../css/index.css'
+import './../css/styles.css'
 
 const csCover = new URL("./../images/CS-cover.jpg",import.meta.url);
 
@@ -7,7 +7,7 @@ export default function Card() {
   return (
     <div className="container">
         <div className="card">
-
+          
             <div className="portrait"></div>
 
             <div className="content">
@@ -17,7 +17,7 @@ export default function Card() {
             <div className="footer">
                 <p>Carlkdsjflsdkfj</p>
             </div>
-            
+          
         </div>
     </div>
   )
